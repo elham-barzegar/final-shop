c060977ea49249de2d547f8a215830d0f288072e
nano byte <elhambarzegar1403@gmail.com>
2024-05-14 19:49:42 +0330

    section and browsCategory

---
+++ src/mock/browsCategory.ts
@@ -0,0 +1,57 @@
+export const browsCategoriesMock = [
+    {
+        icon: "icon-drinks-1",
+        iconPath: 5,
+        title : "Wines & Drinks",
+        a: "#"
+    },
+    {
+        icon: "icon-dress-1",
+        iconPath: 4,
+        title : "Clothing & beauty",
+        a: "#"
+    },
+    {
+        icon: "icon-crab-1",
+        iconPath: 9,
+        title : "Pet Foods & Toy",
+        a: "#"
+    },
+    {
+        icon: "icon-pet-food-1",
+        iconPath: 8,
+        title : "Pet Foods & Toy",
+        a: "#"
+    },
+    {
+        icon: "icon-sweet-1",
+        iconPath: 7,
+        title : "Fast food",
+        a: "#"
+    },
+    {
+        icon: "icon-dairy-products-1",
+        iconPath: 5,
+        title : "Baking material",
+        a: "#"
+    },
+    {
+        icon: "icon-grocery-1",
+        iconPath: 7,
+        title : "Vegetables",
+        a: "#"
+    },
+    {
+        icon: "icon-fruit-1",
+        iconPath: 3,
+        title : "Fresh Fruit",
+        a: "#"
+    },
+    {
+        icon: "icon-milk-products-1",
+        iconPath: 5,
+        title : "Bread and Juice",
+        a: "#"
+    },
+
+]
