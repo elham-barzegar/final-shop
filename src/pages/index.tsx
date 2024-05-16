bbb65498359fc5351ef9108d9a066933f21fb407
nano byte <elhambarzegar1403@gmail.com>
2024-05-16 18:44:23 +0330

    implement productCard , badge and rating component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,47 @@
-import {Banner, FeaturedCategories, MiniProductSlider, Section} from "@/components"
-export default function Home() {
-  return (
-      <>
-   <Section>
-     <Banner title={"Don’t miss amazing grocery deals"}
-             subtitle={"Sign up for the daily newsletter"}
-             image={"/assets/images/fresh-apples.png"}
-             bgImage={"/assets/images/banner_bg.png"}/>
-   </Section>
+import {Banner, FeaturedCategories, IconBox, MiniProductSlider, Section, SimpleProductSlider} from "@/components"
+import {popularProducts} from "@/mock/PopularProducts";
+import {popularFruits} from "@/mock/PopularFruits";
 
-     <section>
-         <div className="hidden sm:flex mb-[50px]">
-             <h2 className="text-heading3 text-blue-300">Featured Categories</h2>
-         </div>
-         <FeaturedCategories/>
-     </section>
-     <section>
-         <MiniProductSlider/>
-     </section>
-          </>
-  )
+export default function Home() {
+    return (
+        <>
+            <Section>
+                <Banner title={"Don’t miss amazing grocery deals"}
+                        subtitle={"Sign up for the daily newsletter"}
+                        image={"/assets/images/fresh-apples.png"}
+                        bgImage={"/assets/images/banner_bg.png"}/>
+            </Section>
+
+            <section>
+                <div className="hidden sm:flex mb-[50px]">
+                    <h2 className="text-heading3 text-blue-300">Featured Categories</h2>
+                </div>
+                <FeaturedCategories/>
+            </section>
+            <section>
+                <MiniProductSlider/>
+            </section>
+
+            <section>
+                <div className="flex justify-between mb-[50px]">
+                    <h2 className="text-heading3 text-blue-300">Popular Products</h2>
+                    <div className="flex items-center gap-3">
+                        <IconBox icon={"swiper-nav-left icon-angle-small-left cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white"} size={24}/>
+                        <IconBox icon={"swiper-nav-right icon-angle-small-right cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white"} size={24}/>
+                    </div>
+                </div>
+                <SimpleProductSlider nextEl={'.swiper-nav-right'} prevEl={'.swiper-nav-left'} sliderData={popularProducts}/>
+            </section>
+            <section>
+                <div className="flex justify-between mb-[50px]">
+                    <h2 className="text-heading3 text-blue-300">Popular Products</h2>
+                    <div className="flex items-center gap-3">
+                        <IconBox icon={"swiper-nav-left2 icon-angle-small-left cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white"} size={24}/>
+                        <IconBox icon={"swiper-nav-right2 icon-angle-small-right cursor-pointer bg-gray-100 p-2 rounded-full text-gray-500 hover:bg-green-200 hover:text-white"} size={24}/>
+                    </div>
+                </div>
+                <SimpleProductSlider nextEl={'.swiper-nav-right2'} prevEl={'.swiper-nav-left2'} sliderData={popularFruits}/>
+            </section>
+        </>
+    )
 }
